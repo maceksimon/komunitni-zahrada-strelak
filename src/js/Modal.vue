617cca4674ddc09ffc9af8fa92802ffa0724256e
Simon Macek <maceksimonlmd@gmail.com>
2022-04-11 17:01:00 +0300

    fix NTF data form + form reset mechanics

--- src/js/Modal.vue
+++ src/js/Modal.vue
@@ -146,6 +146,7 @@
                 Odeslat
               </button>
               <button
+                v-if="formType === 'volunteer' || formType === 'gardener'"
                 type="button"
                 class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                 @click="open = false"
@@ -223,9 +224,12 @@ export default {
         axiosConfig
       );
       if (response.status === 200) {
-        // Reset form and display confirmation
+        // Display confirmation
         formType.value = "submitted";
-        form = formDefault;
+        // Reset form by looping over properties
+        Object.keys(form).forEach((key) => {
+          form[key] = formDefault[key];
+        });
       } else {
         formType.value = "unsuccessful";
       }
